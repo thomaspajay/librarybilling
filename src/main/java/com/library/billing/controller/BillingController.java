b5a950bb3a5d00a09f76a53138301fa243db9ffc
Ajay <ajay@Anjus-MacBook-Pro.local>
2017-05-06 13:05:21 +0530

    Added an interface for Due calculation

--- src/main/java/com/library/billing/controller/BillingController.java
+++ src/main/java/com/library/billing/controller/BillingController.java
@@ -1,8 +1,11 @@
 package com.library.billing.controller;
 
-import java.util.Calendar;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,21 +20,23 @@ public class BillingController {
 	@Autowired
 	BillingService  service ;
 	
+	SimpleDateFormat  format = new SimpleDateFormat("MM-dd-yyyy");
+	
 		@RequestMapping("/")
 		public String home() {
 			return "Library Billiing Service is up !!!";
 		}
 
 		@RequestMapping("/api/v1/billing/{book}")
-		public BillingInfo message(@PathVariable String book, @RequestParam  ("fromDate") String fromDate,
-				@RequestParam  ("toDate") String toDate,@RequestParam  ("price") Double price ) {
-			Calendar cal1 = Calendar.getInstance();
-			cal1.set(2017, 01, 15);
-			Calendar cal2 = Calendar.getInstance();
-			cal2.set(2017, 02, 15);
-			BillingInfo msg = new BillingInfo(book, service.getBilling(cal1.getTime(), cal2.getTime() , null));
+		public BillingInfo getDueforBook(@PathVariable String book, @RequestParam  ("fromDate") String fromDate,
+				@RequestParam  ("toDate") String toDate,@RequestParam  ("price") Double price ) throws ParseException {
+			BillingInfo msg = new BillingInfo(book, service.getBilling(format.parse(fromDate), format.parse(toDate) , price));
 			return  msg;
 		}
+		
+		@RequestMapping("/api/v1/billing/")
+		public ResponseEntity<String> getDue( @RequestParam  ("fromDate") String fromDate,
+				@RequestParam  ("toDate") String toDate,@RequestParam  ("price") Double price ) throws ParseException {
+			return new ResponseEntity<String>("totalDue:"+service.getBilling(format.parse(fromDate), format.parse(toDate) , price),HttpStatus.OK);
+		}
 }
-
-//"fromDate", "toDate", "price"
